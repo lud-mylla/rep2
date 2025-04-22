82ba506fb786e5ec623558ff55f125e7265f825b
lud-mylla <Ludmylla100608@gmail.com>
2025-04-22 18:35:12 +0000

    primeiro commit

---
+++ lista1/exercicio1.py
@@ -0,0 +1,12 @@
+print("Digite dois números: ")
+
+n1 = int(input())
+n2 = int(input())
+
+if n1 > n2:
+    print("Maior =", n1)
+elif n2 > n1:
+    print("Maior =", n2)
+else:
+    print("Os dois números são iguais")
+
---
+++ lista1/exercicio2.py
@@ -0,0 +1,41 @@
+print("Digite quatro valores inteiros:")
+
+n1 = int(input())
+n2 = int(input())
+n3 = int(input())
+n4 = int(input())
+
+media = (n1 + n2 + n3 + n4) // 4
+
+print("Média =", media)
+
+print("Números menores que a média")
+
+if n1 < media:
+    print(n1)
+if n2 < media:
+    print(n2)
+if n3 < media:
+    print(n3)
+if n4 < media:
+    print(n4)
+
+
+print("Números maiores ou iguais a média")
+
+
+if n1 > media:
+    print(n1)
+if n2 > media:
+    print(n2)
+if n3 > media:
+    print(n3)
+if n4 > media:
+    print(n4)
+
+
+
+
+
+
+    
